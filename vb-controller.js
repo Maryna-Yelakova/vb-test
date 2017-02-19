c24d54adfb2a9b631b721030a1b8c2d59d4fb938
maryna.yelakova <maryna.yelakova@gmail.com>
2017-02-19 20:03:04 +0200

    add style to card

--- vb-controller.js
+++ vb-controller.js
@@ -1,16 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
     
     var xhr = new XMLHttpRequest();
-    
+    var meanings = document.getElementById('meanings');
+    var card = document.getElementById('card');
+    var image = document.getElementById('image');
+    var nextMeaning = document.createElement('div');
+    var nextMeaningImage = document.createElement('img');
+    var originWord = document.getElementById('originWord');
+
     function getSelectionText(){
         var selectedText = "";
+        var selected, oRange, oRect;
         if (window.getSelection){
-            selectedText = window.getSelection().toString()
+            selected = window.getSelection();
+            selectedText = selected.toString();
+            oRange = selected.getRangeAt(0); 
+            oRect = oRange.getBoundingClientRect();
         }
-        return selectedText
+        return [selectedText,oRect];
     }
+
+    
     document.addEventListener('mouseup', function(){
-        var text = getSelectionText();
+        var text = getSelectionText()[0];
         if (text.length > 0){
             var textArr = text.split(' ');
             var word;
@@ -26,14 +38,16 @@ document.addEventListener("DOMContentLoaded", function() {
                 xhr.onreadystatechange = function() {
                     if (xhr.readyState == 4) {
                         if(xhr.status == 200) {
-                            console.log(JSON.parse(xhr.responseText));
                             var cardInfo = JSON.parse(xhr.responseText);
-                            var meanings = document.getElementById('meanings');
                             function getMeaningsContent () {
                                 var meaningContent = [];
                                 if (typeof cardInfo !== 'undefined' && cardInfo.length > 0){
                                     for (var i = 0; i< cardInfo[0].meanings.length; i ++ ){
-                                        meaningContent[i] =  cardInfo[0].meanings[i].translation;
+                                        meaningContent[i] =  {
+                                            meaning:cardInfo[0].meanings[i].translation,
+                                            image:cardInfo[0].meanings[i].preview_image_url,
+                                            id:cardInfo[0].meanings[i].id
+                                        };
                                         if(meaningContent[i]==""){
                                             meaningContent[i]= "No translation"
                                         }else{}
@@ -52,13 +66,27 @@ document.addEventListener("DOMContentLoaded", function() {
                             }
                             function getCardResult (){
                                 meanings.innerHTML = '';
+                                nextMeaning.innerHTML = '';
+                                nextMeaningImage.src = '';
+                                originWord.innerHTML = '';
                                 for (var j = 0; j < content.length; j++ ){
-                                    var nextMeaning = document.createElement('div');
-                                    nextMeaning.innerHTML = content[j];
+                                    var item = content[j];
+                                   
+                                    nextMeaning.innerHTML += '<p>'+ item.meaning + '</p>';
                                     meanings.appendChild(nextMeaning);
+                                    nextMeaningImage.src = item.image;
+                                    image.replaceChild(nextMeaningImage, image.childNodes[0]);
+                                    originWord.innerHTML = word;
+                                    nextMeaning.classList.add('translate-word');
+                                    
                                 }
                             }
-                            meanings.classList.add('showCard');
+                            var coordinate = getSelectionText()[1];
+                            console.log(coordinate.top, coordinate.left);
+                            card.style.top = coordinate.top + 'px';
+                            card.style.left = coordinate.left + 'px';
+                            card.classList.remove('hidden');
+                            card.classList.add('showCard');
                         }else {
                             alert( 'Error: ' + (this.status ? this.statusText : 'Query was not successful') );
                         }
